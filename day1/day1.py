68b62e685a41365c09e04ef528b6eb402f95d408
daniel-schneider <daniel.schneider@dfki.de>
2021-12-01 11:44:40 +0100

    add solution for day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -13,3 +13,16 @@ for i in range(len(measurements)):
         print(measurements[i], " is greater than ", measurements[i-1])
         count += 1
 print(count)
+
+# check if sum of i+1,i+2,i+3 is greater than sum of i,i+1,i+2
+slide_count = 0
+for i in range(len(measurements)):
+    try:
+        slide_window_cur = measurements[i]+measurements[i+1]+measurements[i+2]
+        slide_window_sub = measurements[i+1]+measurements[i+2]+measurements[i+3]
+        if slide_window_sub > slide_window_cur:
+            print(slide_window_sub, " is greater than ", slide_window_cur)
+            slide_count += 1
+    except:
+        print("cannot form a subsequent sliding window")
+print(slide_count)
