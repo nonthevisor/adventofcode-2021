6ffb7da079ccdead5b3fa3b0803433d65ae1169b
daniel-schneider <daniel.schneider@dfki.de>
2021-12-02 10:42:33 +0100

    add solution for day 01 part 1

---
+++ day02/day02-part1.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+
+# read input
+with open("input.txt") as file:
+    course = []
+    for line in file:
+        course.append(line.rstrip())
+        
+# calculated position
+horizontal = 0
+vertical = 0
+product = 0
+
+for i in course:
+    i = i.split()
+    # print(i[1]," is type ",type(i[1]))
+    if i[0] == "forward":
+        horizontal += int(i[1])
+    elif i[0] == "up":
+        vertical -= int(i[1])
+    elif i[0] == "down":
+        vertical += int(i[1])
+    else:
+        print("something went wrong")
+
+# multiply horizontal and vertical position
+product = horizontal * vertical
+
+# print results
+print("vertical position is ",vertical)
+print("horizontal position is ",horizontal)
+print("product is ",product)
+
+
+
+
